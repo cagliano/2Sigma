61f5c0e3c7bb0600f49bce4bc68d204753973470
cagliano <paolo.panzeri@gmail.com>
2021-08-16 08:52:31 +0000

    stampa ok

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
+const puppeteer = require("puppeteer");
 var app = express();
 var server = require('http').createServer(app);
 var faker = require('faker');
 var port = process.env.PORT || 3000;
 var minify = require('express-minify');
+
+
+
 
 // user model
 const SigmaRillion = require('./models/Sigma');
@@ -13,42 +17,86 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // routes
 
-app.get('/', function(req,res){
- SigmaRillion.find({})
-	.select('Sigma Orario -_id')
-	.sort('Orario')
-	.limit()
-	.exec(function(err,dati){
-   if (err) { return next(err); }
-   var ValoriSigma = [];
-	ValoriSigma= dati.map(function (obj) {
-  return obj.Sigma;
-});	
+app.get('/', function (req, res) {
+  SigmaRillion.find({})
+    .select('Sigma Orario OrarioFormattato -_id')
+    .sort('Orario')
+    .limit()
+    .exec(function (err, dati) {
+      if (err) { return next(err); }
+      var ValoriSigma = [];
+      ValoriSigma = dati.map(function (obj) {
+        return obj.Sigma;
+      });
 
-//l'orario non è assoluto ma in minuti dalla partenza
-		var ValoriOrario = [];
-// azzero l'array e poi metto come valore 0 il primo rilevamento
-// tutti gli altri valori sono inseriti nell'array come delta dal primo
-		ValoriOrario[0]=0
-for(var i=1; i<dati.length; i++){
-ValoriOrario[i] = (Math.abs(dati[i].Orario-dati[0].Orario)/60000).toFixed(2);
-}
-		res.render('Grafico',{Sigma:ValoriSigma, Orario:ValoriOrario});
-});
+      //l'orario non è assoluto ma in minuti dalla partenza
+      var ValoriOrario = [];
+      // azzero l'array e poi metto come valore 0 il primo rilevamento
+      // tutti gli altri valori sono inseriti nell'array come delta dal primo
+      ValoriOrario[0] = 0
+      for (var i = 1; i < dati.length; i++) {
+        ValoriOrario[i] = (Math.abs(dati[i].Orario - dati[0].Orario) / 60000).toFixed(2);
+      }
+      var sum = 0;
+      for (var i = 0; i < dati.length; i++) {
+        sum += dati[i].Sigma
+      }
+      var SigmaMedio = Math.abs(sum / i).toFixed(2);
+
+
+
+
+      res.render('ProvaBootstrap', { Sigma: ValoriSigma, Orario: ValoriOrario, Inizio: dati[0].OrarioFormattato, Fine: dati[dati.length - 1].OrarioFormattato, Medio: SigmaMedio, EstremoInferiore: 23, EstremoSuperiore: 89 });
+    });
 });
 
 //la pagina generate genera valori a caso per popolare il grafico, ma non dovrebbe servire in futuro
 
 app.get('/generate', (req, res) => {
   for (let i = 0; i < 100; i++) {
-    let Orario=faker.date.recent(),
-        Sigma=faker.random.number({'min': 10,'max': 50})
-    SigmaRillion.create({Orario:Orario, Sigma:Sigma})
-      .then(() => {})
+    let Orario = faker.date.recent(),
+      Sigma = faker.random.number({ 'min': 10, 'max': 50 })
+    SigmaRillion.create({ Orario: Orario, Sigma: Sigma })
+      .then(() => { })
       .catch(err => console.error(err));
   } // end for loop
   res.redirect('/');
 });
+
+app.get("/pdf", async (req, res) => {
+  const url = "http://localhost:3000/";
+  //  const url = "https://getbootstrap.com/";
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+
+  const webPage = await browser.newPage();
+
+  await webPage.goto(url, {
+    waitUntil: "networkidle0"
+  });
+
+  const pdf = await webPage.pdf({
+    printBackground: false,
+    //          landscape: true, // <-- must be set to true to get a landscape PDF
+    landscape: true,
+    format: "A4",
+    path: '/home/ubuntu/2Sigma/landscappa.pdf'
+  });
+
+  await browser.close();
+
+  res.contentType("application/pdf");
+  res.send(pdf);
+})
+
+
+
+
+
+
+
+
 
 server.listen(port, () => console.log(`L'applicazione sta girando sulla porta ${port}`));
 
