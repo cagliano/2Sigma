e14608d6460937a1957b316931ce1726e668999f
cagliano <paolo.panzeri@gmail.com>
2021-08-16 09:42:49 +0000

    con titolo

--- models/Sigma.js
+++ models/Sigma.js
@@ -1,7 +1,17 @@
+const { DateTime } = require("luxon");
+
+
 var mongoose = require('mongoose');
 var SigmaSchema = new mongoose.Schema(
   {
     Orario: {type: Date},
     Sigma: {type: Number},
   });
+
+SigmaSchema
+.virtual('OrarioFormattato')
+.get(function () {
+  return DateTime.fromJSDate(this.Orario).setLocale('fr').toLocaleString(DateTime.DATETIME_MED);
+});
+
 module.exports = mongoose.model('SigmaModello', SigmaSchema);
